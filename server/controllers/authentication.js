dba6685c34da902b693451ef7a667ad73b843e83
Michael <michael@topmiller@gmail.com>
2018-04-23 11:36:43 -0700

    added JWT Auth

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,44 @@
+const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+	res.send({ token: tokenForUser(req.user) });	
+}
+
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true' });
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide an email and password' });
+  }
+
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) {
+      return next(err);
+    }
+
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is already in use' });
+    }
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(err => {
+      if (err) {
+        return next(err);
+      }
+      res.json({ token: tokenForUser(user) });
+    });
+  });
 };
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,43 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
 });
+// run this before saving model
+userSchema.pre('save', function(next) {
+  //get access to the user model
+  const user = this;
+
+  //generate a salt then run callback
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    //hash our password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+      // overwrite inputted password with hash
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+
+    callback(null, isMatch);
+  });
+};
 
 const ModelClass = mongoose.model('user', userSchema);
 
